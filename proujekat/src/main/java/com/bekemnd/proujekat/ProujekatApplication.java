d4d95499e0c6164c0f9d0c84bdbb0624e49486e3
Matke9 <matovicmihailo@gmail.com>
2023-12-02 22:19:02 +0100

    setup gotov

---
+++ proujekat/src/main/java/com/bekemnd/proujekat/ProujekatApplication.java
@@ -0,0 +1,13 @@
+package com.bekemnd.proujekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProujekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProujekatApplication.class, args);
+	}
+
+}
---
+++ proujekat/src/test/java/com/bekemnd/proujekat/ProujekatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bekemnd.proujekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProujekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
