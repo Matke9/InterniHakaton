74509b502d599d33fbfb846e163e034059671a4d
Matke9 <matovicmihailo@gmail.com>
2023-12-04 12:32:00 +0100

    aasd

--- front/src/components/calendar/Cal.jsx
+++ front/src/components/calendar/Cal.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import { Calendar, dateFnsLocalizer } from "react-big-calendar"
 import format from "date-fns/format"
@@ -14,7 +14,7 @@ import './Cal.css'
 const locales = {
     "en-US": require("date-fns/locale/en-US")
 }
-  
+
 const localizer = dateFnsLocalizer({
     format,
     parse,
@@ -22,63 +22,73 @@ const localizer = dateFnsLocalizer({
     getDay,
     locales
 })
-  
+
 const events = [
     {
-      title: "Big Meeting",
-      allDay: true,
-      start: new Date(2023, 11, 3),
-      end: new Date(2023, 11, 4)
+        title: "Ekonomija",
+        allDay: true,
+        start: new Date(2023, 11, 3),
+        end: new Date(2023, 11, 4)
     },
     {
-      title: "Vacation",
-      allDay: true,
-      start: new Date(2023, 11, 4),
-      end: new Date(2023, 11, 9)
+        title: "Marketing",
+        allDay: true,
+        start: new Date(2023, 11, 4),
+        end: new Date(2023, 11, 9)
     },
     {
-      title: "Conference",
-      allDay: true,
-      start: new Date(2023, 11, 10),
-      end: new Date(2023, 11, 10)
+        title: "Programiranje 1",
+        allDay: true,
+        start: new Date(2023, 11, 10),
+        end: new Date(2023, 11, 10)
     }
+
 ]
 
+function addEvents() {
+
+}
+
+
 export const Cal = () => {
-    const [newEvent, setNewEvent] = useState({title: "", start: "", end: ""})
+    const [start, setStart] = useState("")
+    const [end, setEnd] = useState("")
+    const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" })
+    const [newEvent1, setNewEvent1] = useState({ title: "", start: "", end: "" })
+    const [newEvent2, setNewEvent2] = useState({ title: "", start: "", end: "" })
     const [allEvents, setAllEvents] = useState(events)
 
-    function handleAddEvent () {
+    function handleAddEvent() {
         setAllEvents([...allEvents, newEvent])
     }
 
-    return(
+    return (
         <div className="cal-page">
             <div className="calendar">
-                <Calendar 
-                localizer={localizer} 
-                events={allEvents} 
-                startAccessor="start" 
-                endAccessor="end" 
-                style={{height: 500, margin: "50px"}}
+                <Calendar
+                    localizer={localizer}
+                    events={allEvents}
+                    startAccessor="start"
+                    endAccessor="end"
+                    style={{ height: 500, margin: "50px" }}
                 />
             </div>
             <div className="cal-tekst">
                 <h4>Choose subject</h4>
-                <input type="text" placeholder="Add subject" 
-                    value={newEvent.title} 
-                    onChange={(e) => setNewEvent({...newEvent, title: e.target.value})} />
+                <input type="text" placeholder="Add subject"
+                    value={newEvent.title}
+                    onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
 
                 <h4>Starting Date</h4>
                 <DatePicker placeholderText="Start Date"
-                    selected={newEvent.start} onChange={(start) => setNewEvent({...newEvent, start})} />
+                    selected={newEvent.start} onChange={(start) => setNewEvent({ ...newEvent, start })} />
                 <h4>Ending Date</h4>
                 <DatePicker placeholderText="End Date"
-                    selected={newEvent.end} onChange={(end) => setNewEvent({...newEvent, end})} />
+                    selected={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end })} />
 
                 <button onClick={handleAddEvent}>Add Subject</button>
             </div>
         </div>
-        
+
     )
 }
